6b8161839ba16b16dbdad0f59a090f4fcc371484
oilPersonage <oilfreelance@ya.ru>
2020-01-14 18:41:50 +0300

    add three.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
 import '../html/index.html'
 import '../html/photograph.html'
 import '../html/training.html'
+import '../html/about.html'
 import './pubsub'
 import './carousel'
 import './humburger'
 import './photo'
 import './training'
-// import './pixiDepth'
 import './indexLazy'
+import './about'
 
 import '../sass/main.sass'
 
